050a9ca1937d66a733ec8cc82353ffb325182c89
InfernalV <91879230+InfernalV@users.noreply.github.com>
2022-11-17 10:09:08 +0100

    C#0.0.1

---
+++ C#/Yachtapp/Yachtapp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Yachtapp.ViewModels;
+
+namespace Yachtapp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
